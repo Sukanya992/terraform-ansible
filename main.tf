98b1d0ca107da30b471bacfdf33156b1b8bcd08b
Sukanya992 <sukanya09200206@gmail.com>
2025-02-26 15:59:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-medium"
-  zone         = "us-central1-a"
+  zone         = "us-central1-b"
 
   boot_disk {
     initialize_params {
