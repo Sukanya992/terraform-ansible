3637d33933640ad0f6db6c1deb227fd49b45eb28
Sukanya992 <sukanya09200206@gmail.com>
2025-02-26 15:48:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
 provider "google" {
-    credentials = file("key.json")
-    project = "jenkins-452004"
+  project = "jenkins-452004"
+  region  = "us-central1"
+}
 
-}resource "google_compute_instance" "default" {
-  name = "vm1"
-  zone = "us-central1-a"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
   machine_type = "e2-medium"
-  
+  zone         = "us-central1-a"
+
   boot_disk {
     initialize_params {
-      image="debian-12"
+      image = "debian-cloud/debian-11"
     }
   }
+
   network_interface {
     network = "default"
+    access_config {
+    }
   }
 }
