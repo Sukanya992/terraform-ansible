43fb2f05bf539b8c88d08129fe0374d8ed76a23a
Sukanya992 <sukanya09200206@gmail.com>
2025-02-27 16:46:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = "vm"
   machine_type = "e2-medium"
-  zone         = "us-central1-b"
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
